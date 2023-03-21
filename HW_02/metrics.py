a7d3b0adb1d4bd59237e98a9863f25bc6e9366c0
Giacomo Di Prima <giacomo.diprima@studenti.unipd.it>
2023-03-21 21:56:19 +0100

    Implement js-divergence and heatmaps

--- HW_02/metrics.py
+++ HW_02/metrics.py
@@ -1,49 +1,63 @@
 import numpy as np
-import pandas as pd
+import itertools
 
-def pair_error(v, spin=True):
-    if spin:
-        v=(v+1)/2
+
+
+def convert_to_decimal_protein(v):
+    '''
+    Convert the raw protein list of strings of 1s and 0s 
+    to binary numbers
+    '''
+    protein_list = np.copy(v).astype(str)
+    proteins_dataset = np.zeros(protein_list.shape[0]).astype(int)
+
+    for k in np.arange(protein_list.shape[0]):
+        proteins_dataset[k] = int(''.join(protein_list[k]),2)
         
-    np.random.shuffle(v) #shuffle the data
-    list_aa=[]
-    
-    def aa_type(aa):
-        if np.sum(aa*[1,1,0,0]>0):
-            return "P"
-        else:
-            return "A"
-    
-    #variables for the error
-    err_count=0
-    
-    #calculation of the polarity and the error
-    for i in range(len(v)):
-        prec_aa='N'
-        for j in range(5):
-            aa=v[i, 4*j : 4*j+4]
-            aa=aa_type(aa)
-            list_aa.append(aa)
-            if(aa==prec_aa):
-                err_count+=1
-            prec_aa=aa
-            
-    return err_count/(4*len(v))*100
+    return list(proteins_dataset)
 
-def sequence_error(v1, spin=True, vmin=-1):
-    if spin:
-        v1=np.array((v1-vmin)/(1-vmin), dtype=int)
-    df=pd.DataFrame(v1).astype('int')
+def return_protein_probabilities(decimal_proteins_list, n_amminoacids=5):
+    '''
+    Returns a pdf. Each element of the returned vector
+    represents the probability of each one of the 4^5 proteins
+    in the list passed to the function.
+    
+    The output vector length is always 4^5=1024, independently of
+    the input proteins list passed as input.
+    '''
+    #number of amminoacids in the protein
+    n_ammino=n_amminoacids
+    
+    #total number of preteins, to normalize the pdf
+    n_proteins=len(decimal_proteins_list)
 
-    error=np.zeros(len(df))
-    for i in range(len(df)):
-        amminoacids = np.reshape(np.array(df.loc[i]), (5,4))
-        for a in range(4):
-            if list(np.where(amminoacids[a,:]))[0][0] <2 and list(np.where(amminoacids[a+1,:]))[0][0] <2 :
-                error[i]=1
-            elif list(np.where(amminoacids[a,:]))[0][0] >1 and list(np.where(amminoacids[a+1,:]))[0][0] >1:
-                error[i]=1
-    return sum(error)*100/len(df)
+    #encoding of amminoacids
+    v1,v2,v3,v4=np.eye(4)
+
+    #list all possible combinations of proteins
+    possibilities=list(itertools.product(['1000','0100','0010','0001'],
+                                         repeat=n_ammino))
+
+    #converts proteins to decimal representation
+    decimal=list()
+    for p in possibilities:
+        decimal.append(int(''.join(p),2))
+
+    #sort the representation from bigger to smaller values
+    decimal.sort(reverse=True)
+
+    #initialize a dict with proteins as keys
+    protein_cnt=dict.fromkeys(decimal,0)
+
+    #counts the number of times the protein was found
+    for protein in decimal_proteins_list:
+        protein_cnt[protein]+=1 
+
+    #return the ordered list of 
+    #normalized probabilities for each possible protein
+    norm_prob=np.array(list(protein_cnt.values()))/n_proteins
+    
+    return norm_prob
 
 def KL_divergence(p, q):
     pp = np.copy(p)
@@ -54,11 +68,3 @@ def JS_divergence(p,q):
     m = 0.5*(p+q)
     js = 0.5*(KL_divergence(p, m) + KL_divergence(q, m))
     return js
-
-def p(v, spin=True):
-    
-    if spin:
-        vp = (v+1)/2 
-        
-    p = np.sum(vp, axis=0)/v.shape[0]
-    return p
